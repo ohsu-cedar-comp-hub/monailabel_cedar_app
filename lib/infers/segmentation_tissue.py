dea9be93ada0a8cb5ae11887404e158d39d98444
guanmingwu <guanmingwu@gmail.com>
2024-10-30 09:29:42 -0700

    added a hack for ome.tif image files

--- lib/infers/segmentation_tissue.py
+++ lib/infers/segmentation_tissue.py
@@ -25,6 +25,7 @@ import monai
 import monai.data
 import numpy as np
 import skimage
+from tifffile import TiffFile, imwrite
 import yaml
 import torch
 
@@ -38,12 +39,11 @@ from monai.transforms import (
     EnsureType,
     CastToTyped,
 )
-from monai.data.image_reader import PILReader
+from monai.data.image_reader import PILReader, ITKReader
 from torch.cuda.amp import autocast
 from inference.utils.monai_utils import sliding_window_inference
 
 import inference
-
 
 logger = logging.getLogger(__name__)
 
@@ -187,6 +187,7 @@ class SegmentationTissueInferTask(InferTask):
 
 
     def map_to_class(self, multiclass_mask, save_path: Union[str, Path] = None):
+        # For some reason, we have to make a copy here, which is not a good use of memory
         binary_mask = copy.deepcopy(multiclass_mask)
         binary_mask[multiclass_mask > 0] = 255
 
@@ -262,6 +263,10 @@ class SegmentationTissueInferTask(InferTask):
         masked_image = None
         with torch.no_grad():
             for _, batch_data in enumerate(test_loader):
+                # A hard fix
+                if batch_data['image'].shape[1] == 4:
+                    batch_data['image'] = batch_data['image'][:, :3, :, :]
+
                 if self.args.infer_only:
                     labels_l = None
                 else:
@@ -299,19 +304,65 @@ class SegmentationTissueInferTask(InferTask):
                     )
 
                 # Need to check if this is useful in this context
-                y_pred = torch.stack(post_pred(monai.data.utils.decollate_batch(val_outputs)))
+                # y_pred = torch.stack(post_pred(monai.data.utils.decollate_batch(val_outputs)))
 
                 if self.args.save_infer:
-                    val_outputs = val_outputs.cpu() * torch.cat([torch.zeros(1, 1, *val_outputs.shape[-2:]),
-                                                                    torch.ones(1, val_outputs.shape[1]-1, *val_outputs.shape[-2:])], dim=1)
-                    val_outputs = (val_outputs > 0.5).half() * val_outputs
-                    save_np = np.transpose(torch.argmax(val_outputs, dim=1).squeeze().cpu().numpy().astype(np.uint8))
-                    masked_img_file = os.path.join(self.args.logdir, file_name+"_pred.tif")
+                    # Ensure val_outputs is on GPU
+                    # Define masking tensor only once to save memory
+                    # Ensure val_outputs is on GPU
+                    
+                    # Step 1: Set the first channel to zero in-place, mask the remaining channels above the threshold of 0.5
+                    val_outputs[:, 0] = 0  # Set the first channel to zero
+                    val_outputs[:, 1:] = (val_outputs[:, 1:] > 0.5).half() * val_outputs[:, 1:]  # Threshold and mask other channels in-place
+
+                    # Step 2: Perform argmax on GPU to get the output mask
+                    save_np = torch.argmax(val_outputs, dim=1).squeeze().cpu().numpy().astype(np.uint8)  # Move to CPU for saving
+
+                    # Step 3: Transpose and save as image
+                    save_np = np.transpose(save_np)
+                    masked_img_file = os.path.join(self.args.logdir, file_name + "_pred.tif")
                     cv2.imwrite(masked_img_file, save_np)
                     logger.info('Saved masked image: {}'.format(masked_img_file))
-                    #TODO: Need to check if val_outputs can be used by the class mapping
+
+                    # Final result for further processing (still on CPU as numpy)
                     masked_image = save_np
+
+                    # CPU version
+                    # val_outputs = val_outputs.cpu() * torch.cat([torch.zeros(1, 1, *val_outputs.shape[-2:]),
+                    #                                              torch.ones(1, val_outputs.shape[1]-1, *val_outputs.shape[-2:])], dim=1)
+                    # val_outputs = (val_outputs > 0.5).half() * val_outputs
+                    # save_np = np.transpose(torch.argmax(val_outputs, dim=1).squeeze().cpu().numpy().astype(np.uint8))
+                    # masked_img_file = os.path.join(self.args.logdir, file_name+"_pred.tif")
+                    # cv2.imwrite(masked_img_file, save_np)
+                    # logger.info('Saved masked image: {}'.format(masked_img_file))
+                    # #TODO: Need to check if val_outputs can be used by the class mapping
+                    # masked_image = save_np
         return masked_image
+
+
+    def handle_ome_tiff(self,
+                        src_image_path: Path) -> Path:
+        """Dump a tiff image file first from the ome.tiff file. It would be nicer to add an image reader
+           to handle this action instead of this hacking way.
+        Args:
+            src_image_path (Path): the source image in the format ome.tiff
+
+        Returns:
+            Path: the path to the generated image
+        """
+        logger.info('Exporting the largest image in {}...'.format(src_image_path))
+        # Get the path
+        file_name = src_image_path.name.replace(".ome.tif", ".tif")
+        output_path = Path(self.args.logdir, file_name)
+        with TiffFile(src_image_path) as tif:
+            # Access image series within the OME-TIFF
+            images = [page.asarray() for page in tif.pages]
+
+            # Find the largest image by area (width * height)
+            largest_image = max(images, key=lambda img: img.shape[1] * img.shape[2])
+            # Dump the largest image
+            imwrite(output_path, largest_image)
+            return output_path
 
 
     def build_dataloader(self,
@@ -320,11 +371,16 @@ class SegmentationTissueInferTask(InferTask):
         logger.info('Processing the image file: {}/{}'.format(src_image_dir, src_image_file))
 
         src_image_path = Path(src_image_dir, src_image_file)
+        if src_image_file.endswith('.ome.tif') or src_image_file.endswith('.ome.tiff'):
+            src_image_path = self.handle_ome_tiff(src_image_path)
+
         test_files = [{'image': str(src_image_path)}]
         
         keys = ["image"]
         val_transforms = Compose([
+            # Need to use this reader for old tiff files
             LoadImaged(keys=keys, reader=PILReader, image_only=True),
+            # LoadImaged(keys=keys, reader=ITKReader, image_only=True),
             EnsureChannelFirstd(keys=keys),
             CastToTyped(keys=keys, dtype=[torch.uint8] if self.args.infer_only else [torch.uint8, torch.uint8]),
         ])
